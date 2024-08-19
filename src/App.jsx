ca9ec3617a22ece267d19862dededfff0c8f2d8f
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-19 13:36:26 -0600

    finished parts 2 and 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-function BaseballCard(props) {
+import playerData from "./playerData";
+
+function BaseballCard({name, team, position, stats, imgUrl, cardId}) {
+  let statsDisplay = []
+  for (const [key, value] of Object.entries(stats)) {
+    statsDisplay.push(
+      <p key={key}>
+        {key}: {value}
+      </p>
+    )
+  }
+
   return (
     <div className="card">
-      <h2>Player Name goes here</h2>
+      <h2>{name}</h2>
+      <p>Team: {team}</p>
+      <p>Position: {position}</p>
+      {statsDisplay}
+
     </div>
-  );
+  )
+  // return (
+  //   <div className="card">
+  //     <h2>{name}</h2>
+  //     <img src={imgUrl} alt={`Picture of ${name}`} />
+  //   </div>
+  // );
 }
 
 function App() {
-  return <BaseballCard />;
+  const cards = playerData.map((player) => <BaseballCard name={player.name} team={player.team} position={player.position} stats={player.stats} imgUrl={player.imgUrl} cardId={player.cardId}/>)
+  return <>{cards}</>;
 }
 
 export default App;
